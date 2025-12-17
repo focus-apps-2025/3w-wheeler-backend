18260a7788749e30fed3190862c8139caa436a6a
focusengineeringforms <focusengg123@gmail.com>
2025-12-17 23:42:32 +0530

    updated

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ extended: true, limit: "200mb" }));
 
 
 app.use('/api/pdf', pdfRoutes);
